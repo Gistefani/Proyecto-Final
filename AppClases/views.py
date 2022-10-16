d15d11200cad0e230a873bfb6884078845171db0
Gistefani <stefanigise@gmai.com>
2022-10-16 09:09:10 -0300

    se agregan las funciones a las view de la AppCLases

--- AppClases/views.py
+++ AppClases/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def inicio(request):
+    return render (request,"inicio.html")
+
+
+def Estilos(request):
+    return render (request,"Estilos.html")
+
+def Profesores(request):
+    return render (request,"Profesores.html")
+
+def Estududiante(request):
+    return render (request,"Estudiante.html")            
+
+
+
+
