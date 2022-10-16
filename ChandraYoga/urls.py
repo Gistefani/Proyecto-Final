fa78e9d4ef0b0a99e2952b8e061a3d47e927d688
Gistefani <stefanigise@gmai.com>
2022-10-16 09:10:56 -0300

    se agregan los path de las Apps que agrego mediante el include para que lo busque en las url de cada App

--- ChandraYoga/urls.py
+++ ChandraYoga/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
 from ChandraYoga.views import home, homePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
+    path('AppClases/', include("AppClases.urls")),
+    path('AppEstilos/',include("AppEstilos.urls")),
+    
 ]
