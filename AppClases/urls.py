495b9c12d6a8f4c9a26299b22709948d0706cc88
Gistefani <stefanigise@gmai.com>
2022-10-18 18:52:52 -0300

    registro las urls de los crud que cree

--- AppClases/urls.py
+++ AppClases/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('Estudiantes/', Estudiantes),
     path('Profesores/', Profesores),
     path('api_estudiantes/', api_estudiantes),
-    path('buscar_estudiante/', buscar_estudiante)
+    path('buscar_estudiante/', buscar_estudiante),
+    path('create_estudiante/', create_estudiante),
+    path('delete_estudiante/', delete_estudiante),
+    path('read_estudiante/', read_estudiante),
+    path('update_estudiante/', update_estudiante)
 ]
 
 
