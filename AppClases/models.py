ee9bdff2cd00e0a5410b55073946e451c17f5188
Gistefani <stefanigise@gmai.com>
2022-10-18 18:44:16 -0300

    agrego funciones a mis clases

--- AppClases/models.py
+++ AppClases/models.py
@@ -8,13 +8,22 @@ class Estilo (models.Model):
     nivel = models.CharField (max_length=10)
     horarios = models.TimeField()
 
-class Profesores(models.Model):
-    nombre = models.CharField (max_length=20)
-    apellido = models.CharField (max_length=35)
-    estilo = models.CharField (max_length=20)
+    def __str__(self):
+        return f"nombre:{self.nombre} - Nivel:{self.nivel} - Horario:{self.horarios}"
 
-class Estudiantes (models.Model):
-    nombre = models.CharField (max_length=40)
-    apellido = models.CharField (max_length=40)
+class Profesores(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=35)
+    estilo = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"nombre:{self.nombre} - Apellido:{self.apellido} - email:{self.estilo}"
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     email = models.EmailField()
+    
+    def __str__(self):
+        return f"nombre:{self.nombre} - Apellido:{self.apellido} - email:{self.email}"
     
