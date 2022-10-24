71093218fe49fe992f7f65518ac2b24842d3ca52
Gistefani <stefanigise@gmai.com>
2022-10-23 22:05:55 -0300

    agrego las urls para las nuevas templates y las views con sus funciones

--- AppClases/urls.py
+++ AppClases/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from AppClases.views import *
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', inicio),
+    path('Home/', Home),
     path('Estilos/', Estilos),
     path('Estudiantes/', Estudiantes),
     path('Profesores/', Profesores),
@@ -11,7 +12,10 @@ urlpatterns = [
     path('create_estudiantes/', create_estudiantes),
     path('delete_estudiantes/<estudiante_email>', delete_estudiantes),
     path('read_estudiantes/', read_estudiantes),
-    path('update_estudiantes/<estudiante_email>', update_estudiantes)
+    path('update_estudiantes/<estudiante_email>', update_estudiantes),
+    path('login/',login_request),
+    path('registro/',registro),
+    path('logout/',LogoutView.as_view(template_name = 'home.html'),name="logout" )
 ]
 
 
--- AppClases/views.py
+++ AppClases/views.py
@@ -1,11 +1,25 @@
 
 import email
-from django.shortcuts import render
+import django
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from AppClases.models import *
-from AppClases.forms import form_estudiantes
+from AppClases.forms import UserRegisterForm , form_estudiantes
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import login,logout,authenticate
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def Home(request):
+    return render (request,"Home.html")
+
 
 def inicio(request):
     return render (request,"inicio.html")
@@ -15,7 +29,7 @@ def Estilos(request):
 
 def Profesores(request):
     if request.method == "POST":
-       profesor = Profesores(nombre = request.POST['Nombre'], apellido = request.POST['Apellido'], Estilo = request.POST['Estilo'])
+       profesor = Profesores(nombre =request.POST['Nombre'], apellido = request.POST['Apellido'], Estilo = request.POST['Estilo'])
        profesor.save()
        return render (request,"inicio.html")  
     return render (request,"Profesores.html")
@@ -87,4 +101,40 @@ def delete_estudiantes(request,estudiante_email):
     estudiante.delete()
     estudiantes = Estudiantes.objects.all()  ##aca va Estudiantes.objects.all()
     return render(request ,"estudiantesCRUD/read_estudiantes.html", {"estudiantes":estudiantes} )
-  
+
+
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm (request, data = request.POST)
+        if form.is_valid():
+            user = form.cleaned_data.get('username')
+            pwd = form.cleaned_data.get('passward')
+
+            user = authenticate(username = user , password = pwd)
+
+            if user is not None:
+                login(request,user)
+                return render (request, "inicio.html")
+            else:
+                return render (request,"login.html", {'form':form})
+        else:
+                return render (request,"login.html", {'form':form})  
+    form = AuthenticationForm()
+    return render (request,'login.html', {'form': form})
+
+
+def registro(request):
+     if request.method == 'POST':
+           form = UserRegisterForm(request.POST)
+           if form.is_valid():
+                  #username = form.cleaned_data["username"]
+                  form.save()
+                  return redirect("/AppClases/login/")     
+
+     else:           
+          form = UserRegisterForm() 
+          return render(request,"registro.html",{'form': form})
+ 
+
